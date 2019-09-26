004d20d2019685388502cfaf947579ba60f37f80
neha.patil <neha.patil@zeuslearning.com>
2019-09-26 12:05:22 +0530

    implementaion of async await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,12 +1,25 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5d8a580bcd7a0f296c2ee448').then((tasks) => {
-    console.log(tasks)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
+//Promise chaining
+// Task.findByIdAndDelete('5d8a580bcd7a0f296c2ee448').then((tasks) => {
+//     console.log(tasks)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+//async await
+const deleteAndCount = async (id) => {
+    const tasks = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteAndCount('5d8b1ef8fd7e012ec4ab1385').then((result) => {
     console.log(result)
 }).catch((e) => {
     console.log(e)
 })
-    
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,25 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5d877f4efcb5d832f48869c3', { age: 11 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 11 })
-}).then((count) => {
-    console.log(count)
+//Promise chaining
+// User.findByIdAndUpdate('5d877f4efcb5d832f48869c3', { age: 11 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 11 })
+// }).then((count) => {
+//     console.log(count)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+//async await 
+const upadateAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+upadateAndCount('5d8c57a08a38192ae8d19ab6', 11).then((result) => {
+    console.log(result)
 }).catch((e) => {
     console.log(e)
 })
