d98e45b24ebabc8d0e3accb4fdfabecf1c9cdd73
neha.patil <neha.patil@zeuslearning.com>
2019-10-09 12:10:42 +0530

    Addition of timestamps to user and task model

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
-const Task = mongoose.model('Task', {
-    description:{
+const taskSchema = new mongoose.Schema({
+    description: {
         type: String,
         required: true,
         trim: true
@@ -10,11 +10,15 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     },
-    owner:{
+    owner: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref:'User'
+        ref: 'User'
     }
+}, {
+    timestamps: true
 })
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
--- src/models/user.js
+++ src/models/user.js
@@ -40,6 +40,8 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+},{
+    timestamps: true
 })
 userSchema.virtual('tasks', {
     ref: 'Task',
