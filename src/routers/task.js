fd17edfafbe9278f9de5c7dc5693cf9e85b85777
neha.patil <neha.patil@zeuslearning.com>
2019-10-10 12:26:24 +0530

    Pagination and sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,18 +18,31 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 //GET /tasks?completed=true
+//GET /tasks?limit=10&&skip=10
+//GET /tasks?sortBy=createdAt:dec
 router.get('/tasks', auth, async (req, res) => {
     try {
         // const tasks = await Task.find({ owner: req.user._id })
         const match = {}
+        const sort = {}
 
         if (req.query.completed) {
-            match.completed = req.query.completed === 'true' 
+            match.completed = req.query.completed === 'true'
+        }
+
+        if (req.query.sortBy) {
+            const parts = req.query.sortBy.split(":")
+            sort[parts[0]] = parts[1] === 'dec' ? -1 : 1
         }
 
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
