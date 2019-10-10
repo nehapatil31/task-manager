c7fb2ba60cf95f3cb90481de614196ff799da5b7
neha.patil <neha.patil@zeuslearning.com>
2019-10-10 13:27:55 +0530

    multer WIP

--- src/index.js
+++ src/index.js
@@ -18,7 +18,14 @@ app.use(taskRouter)
 
 app.listen(port, () => {
     console.log("Server is up on port " + port)
-    debugger
+})
+
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
 })
 
 // myfunction = async () => {
